f825c0d916c9ac20fca55e3ec7cc50ffc7c73197
Rafael Valiullin <vrafael@mail.ru>
2017-04-01 23:01:55 +0300

    no message

--- StoredProcedures/API.FundGet.sql
+++ StoredProcedures/API.FundGet.sql
@@ -17,6 +17,8 @@ BEGIN
     DECLARE
         @TypeID_AccountFund bigint = dbo.TypeIDByName('AccountFund')
        ,@StateID_Accepted bigint = dbo.DirectoryIDByOwner(NULL, 'StateSchemeInvite', NULL, 'State', 'Rejected')
+       ,@StateID_Sended bigint = dbo.DirectoryIDByOwner(NULL, 'StateSchemeInvite', NULL, 'State', 'Sended')
+       ,@StateID_Opened bigint = dbo.DirectoryIDByOwner(NULL, 'StateSchemeFund', NULL, 'State', 'Opened')
 
     SELECT
         f.ID as FundID
@@ -24,10 +26,22 @@ BEGIN
        ,otl.Title as FounderTitle
        ,f.Caption as FundCaption
        ,f.[Description] as FundDescription
+       ,ISNULL(sd.Name, 'Created') as FundStateName
+       ,ISNULL(sd.Caption, 'Создан') as FundStateCaption
        ,CAST(atrin.Amount as nvarchar(10)) + ' ' + cd.Name as FundAmount
        ,CAST(NULLIF(ISNULL(atrin.Amount, 0) - ISNULL(atrout.Amount, 0), 0) as nvarchar(10)) + ' ' + cd.Name as FundBalance
-       ,ISNULL(inv.InviteCount, 0) as InviteCount
+       ,ISNULL(invall.InviteCount, 0) as InviteCount
+       ,CAST(1 as bit) as ButtonFinish
+       ,CAST(1 as bit) as ButtonInviteAccept
+       ,CAST(1 as bit) as ButtonInviteReject
+       ,CAST(1 as bit) as ButtonPay
+       --,CAST(IIF(@PersonID = f.FounderID AND o.StateID = @StateID_Opened, 1, 0) as bit) as ButtonFinish
+       --,CAST(IIF(invpers.InviteStateID = @StateID_Sended, 1, 0) as bit) as ButtonInviteAccept
+       --,CAST(IIF(invpers.InviteStateID IN (@StateID_Sended, @StateID_Accepted), 1, 0) as bit) as ButtonInviteReject
+       --,CAST(IIF(o.StateID = @StateID_Opened, 1, 0) as bit) as ButtonPay
     FROM dbo.TFund f
+        JOIN dbo.TObject o ON o.ID = f.ID
+        LEFT JOIN dbo.TDirectory sd ON sd.ID = o.StateID
         OUTER APPLY dbo.ObjectTitleList(f.FounderID) otl
         LEFT JOIN dbo.TAccount a
             JOIN dbo.TObject ao ON a.ID = ao.ID AND ao.TypeID = @TypeID_AccountFund
@@ -55,7 +69,17 @@ BEGIN
                 JOIN dbo.TObject o ON o.ID = i.ID
             WHERE i.FundID = f.ID
                 AND o.StateID = @StateID_Accepted
-        ) inv
+        ) invall
+        OUTER APPLY
+        (
+            SELECT TOP (1)
+                o.StateID as InviteStateID
+            FROM dbo.TInvite i
+                JOIN dbo.TObject o ON o.ID = i.ID
+            WHERE i.FundID = f.ID
+                AND o.StateID IN (@StateID_Sended, @StateID_Accepted)
+            ORDER BY i.ID DESC
+        ) invpers
     WHERE f.ID = @FundID
 END
 --EXEC API.FundGet @FundID = 561345, @PersonID = 561220
--- StoredProcedures/API.Funds.sql
+++ StoredProcedures/API.Funds.sql
@@ -14,19 +14,26 @@ BEGIN
     SET NOCOUNT ON;
 
     DECLARE
-        @TypeID_AccountFund bigint = dbo.TypeIDByName('AccountFund');
+        @TypeID_AccountFund bigint = dbo.TypeIDByName('AccountFund')
+       ,@StateID_Finished bigint = dbo.DirectoryIDByOwner(NULL, 'StateSchemeFund', NULL, 'State', 'Finished')
+       ,@StateID_Sended bigint = dbo.DirectoryIDByOwner(NULL, 'StateSchemeInvite', NULL, 'State', 'Sended')
+       ,@StateID_Accepted bigint = dbo.DirectoryIDByOwner(NULL, 'StateSchemeInvite', NULL, 'State', 'Accepted');
 
     WITH Funds AS
     (
         SELECT
             f.ID as FundID
         FROM dbo.TFund f
+            JOIN dbo.TObject o ON o.Id = f.ID
         WHERE f.FounderID = @PersonID
+            AND (o.StateID IS NULL OR o.StateID <> @StateID_Finished) -- Фонд не закрыт
         UNION
         SELECT
             i.[FundID]
         FROM dbo.TInvite i
+            JOIN dbo.TObject o ON o.ID = i.ID
         WHERE i.[InviteeID] = @PersonID
+            AND o.StateID IN (@StateID_Sended, @StateID_Accepted) -- Приглашение в фонд Отправлено или Принято
     )
     SELECT
         f.ID as FundID
@@ -34,9 +41,13 @@ BEGIN
        ,otl.Title as FounderTitle
        ,f.Caption as FundCaption
        ,f.[Description] as FundDescription
+       ,ISNULL(sd.Name, 'Created') as FundStateName
+       ,ISNULL(sd.Caption, 'Создан') as FundStateCaption
        ,CAST(atr.Amount as nvarchar(10)) + ' ' + cd.Name as FundAmount
     FROM Funds ff
         JOIN dbo.TFund f ON f.ID = ff.FundID
+        JOIN dbo.TObject o ON o.ID = f.ID
+        LEFT JOIN dbo.TDirectory sd ON sd.ID = o.StateID
         OUTER APPLY dbo.ObjectTitleList(f.FounderID) otl
         LEFT JOIN dbo.TAccount a
             JOIN dbo.TObject ao ON a.ID = ao.ID AND ao.TypeID = @TypeID_AccountFund
